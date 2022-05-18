7ccc92ce0236a091adea45157079c9f95049a5fc
s3812496 <s3812496@student.rmit.edu.au>
2022-05-18 10:59:33 +1000

    Completed ticket Class

---
+++ Final_Assesment /src/Ticket.java
@@ -0,0 +1,85 @@
+public class Ticket {
+    static protected String name;
+    static protected int price;
+    static protected int quantity;
+
+    // Default constructor
+    public Ticket() {
+        name = "UNKNOWN";
+        price = -1;
+        quantity = 0;
+    }
+
+    public Ticket(String name, int price, int quantity) {
+        // Checks to see if a name exists if not it does not set the name
+        if (name != null) {
+            Ticket.name = name;
+        }
+
+        // Checks to see if price is 0 or greater and sets it, otherwise it throws
+        // an exception
+        if (price >= 0) {
+            Ticket.price = price;
+        } else {
+            throw new IllegalArgumentException(price + " is less than 0, price can not bee less than 0");
+        }
+
+        if (quantity >= 1) {
+            Ticket.quantity = quantity;
+        } else {
+            throw new IllegalArgumentException(quantity + " is less than 1, quantity can not be less than 1");
+        }
+    }
+
+
+    // Checks name is valid and sets
+    public void setName(String name) {
+        if (name != null) {
+            Ticket.name = name;
+        }
+    }
+
+
+    // Returns name
+    public String getName(){return Ticket.name;}
+
+
+    // Checks price is valid and sets
+    public void setPrice(int price){
+        if (price >= 0) {
+            Ticket.price = price;
+        } else {
+            throw new IllegalArgumentException(price + " is less than 0, price can not bee less than 0");
+        }
+    }
+
+
+    // Returns price
+    public int getPrice(){return Ticket.price;}
+
+
+    // Checks quantity is valid and sets
+    public void setQuantity(int quantity) {
+        if (quantity >= 1) {
+            Ticket.quantity = quantity;
+        } else {
+            throw new IllegalArgumentException(quantity + " is less than 1, quantity can not be less than 1");
+        }
+    }
+
+
+    // Returns quantity
+    public int getQuantity(){return Ticket.quantity;}
+
+
+    // Returns total price
+    public int getTotalPrice(){return (Ticket.price * Ticket.quantity);}
+
+
+    // Prints out the information of this object
+    public String toString() {
+        return (Ticket.name + " " + Ticket.quantity + " X $" + Ticket.price + " = $" + getTotalPrice());
+    }
+
+
+}
