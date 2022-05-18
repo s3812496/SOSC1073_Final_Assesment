a5dd8933b51ba2af8e6190ee64fc23d952f62f6c
s3812496 <s3812496@student.rmit.edu.au>
2022-05-18 16:23:22 +1000

    Filling out class

--- Final_Assesment/src/ShoppingCart.java
+++ Final_Assesment/src/ShoppingCart.java
@@ -1,3 +1,5 @@
+import java.util.Locale;
+
 public class ShoppingCart {
 
     // Private Variables
@@ -42,10 +44,74 @@ public class ShoppingCart {
         this.date = date;
     }
 
+
+    // This method validates the entry of a ticket in to the cart then returns true or false
     public boolean add(Ticket ticket) {
+        // Check if ticket passed is null be looking for default name
+        if (ticket.getName() == "UNKNOWN") {
+            System.out.println("Ticket invalid or already added.");
+            return false;
+        } else {
+
+            // Run through array and check if ticket already exists
+            for (int i = 0; i <= inCartTickets.length; i++) {
+
+                // Compare name in the input compared to array
+                if (ticket.getName().toLowerCase() == inCartTickets[i].getName().toLowerCase()) {
+                    System.out.println("Ticket invalid or already added.");
+                    return false;
+                } else {
+
+                    // Finding if array is full and if it is returning false
+                    if (inCartTickets[inCartTickets.length].getName() != null) {
+                        System.out.println("SHOPPING CART IS FULL");
+                        return false;
+                    } else {
+
+                        // Find the first null entry and place the record in it
+                        for (i = 0; i <= inCartTickets.length; i++) {
+
+                            // Using IF statement to find the first empty
+                            if (inCartTickets[i].getName() == null) {
+                                inCartTickets[i] = ticket;
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
 
 
-
+        }
         return false;
+        }
+
+        // This method runs through all the shopping cart entries and adds up the quantity
+        public int getTotalCount() {
+            int TotalCount = 0;
+
+            // Run through a for loop and add the quantity up for all the tickets in the cart
+            for (int i = 0; i <= inCartTickets.length; i++) {
+                TotalCount = TotalCount + inCartTickets[i].getQuantity();
+            }
+
+        return TotalCount;
+    }
+
+    // This Method adds up the total cost of all tickets by calling the total cost method in a loop
+    public int getCost(){
+        int totalCost = 0;
+
+        // for every entry call up the get price method and add it to the total
+        for (int i = 0; i <= inCartTickets.length; i++){
+            totalCost = totalCost + inCartTickets[i].getPrice();
+        }
+
+        return totalCost;
+    }
+
+    // This method prints the information of the shopping cart and prints "Shopping cart is empty" if no records exist
+    public void printTotal(){
+        
     }
 }
